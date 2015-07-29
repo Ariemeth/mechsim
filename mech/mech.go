b273dc589240372fd33c181aa815663263e9068c
Edward Carmack <AriemethAcc@gmail.com>
2015-07-28 23:16:09 -0400

    made the mech class private to force use of the NewMech method

--- mech/mech.go
+++ mech/mech.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Mech is a basic mech type
-type Mech struct {
+type mech struct {
 	structure    int
 	maxStructure int
 	weapons      []Weapon
@@ -14,22 +14,18 @@ type Mech struct {
 }
 
 // NewMech is used to create a new instance of a mech with default structure.
-func NewMech(name string, maxStructure int) *Mech {
-	newMech := new(Mech)
-	newMech.structure = maxStructure
-	newMech.maxStructure = maxStructure
-	newMech.name = name
-
-	return newMech
+func NewMech(name string, maxStructure int) *mech {
+	return &mech{name: name, structure: maxStructure,
+		maxStructure: maxStructure}
 }
 
 // StructureLeft Retrieves the amount of remaining structure a mech has.
-func (mech Mech) StructureLeft() int {
+func (mech mech) StructureLeft() int {
 	return mech.structure
 }
 
 // internal call when a mech is hit
-func (mech *Mech) hit(damage int) {
+func (mech *mech) hit(damage int) {
 	mech.structure -= damage
 	fmt.Println(mech.name, "takes", damage, "damage")
 	if mech.structure <= 0 {
@@ -38,12 +34,12 @@ func (mech *Mech) hit(damage int) {
 }
 
 // AddWeapon adds a Weapon to the mech
-func (mech *Mech) AddWeapon(weapon Weapon) {
+func (mech *mech) AddWeapon(weapon Weapon) {
 	mech.weapons = append(mech.weapons, weapon)
 }
 
 // Fire tells the Mech to fire at a Target
-func (mech *Mech) Fire(rangeToTarget int, target Target) {
+func (mech *mech) Fire(rangeToTarget int, target Target) {
 	for _, weapon := range mech.weapons {
 		weapon.Fire(rangeToTarget, target)
 	}
